e7c8f28a6f9560f3418924f52c19e68b207f3916
Vladar <vladar4@gmail.com>
2012-12-11 18:07:09 +0200

    further unicode optimization

--- libtcod/console.nim
+++ libtcod/console.nim
@@ -147,54 +147,64 @@ proc console_print_frame*(con: PConsole, x, y, w, h: int, empty: bool, flag=BKGN
 when not NO_UNICODE:
   when defined(linux):
     import unicode
+
     # modified for linux (int32) WideCString from Nimrod's system/widestrs.nim
 
     type
       TUTF32Char = distinct int32
       WideCString = ptr array[0..1_000_000, TUTF32Char]
 
-    const
-      UNI_REPLACEMENT_CHAR = TUTF32Char(0xFFFD'i32)
-      UNI_MAX_BMP = TRune(0x0000FFFF)
-      UNI_MAX_UTF32 = TRune(0x7FFFFFFF)
-      #UNI_MAX_LEGAL_UTF32 = TRune(0x0010FFFF)
-      halfShift = 10
-      halfBase = 0x0010000
-      halfMask = 0x3FF
-      UNI_SUR_HIGH_START = TRune(0xD800)
-      #UNI_SUR_HIGH_END = TRune(0xDBFF)
-      UNI_SUR_LOW_START = TRune(0xDC00)
-      UNI_SUR_LOW_END = TRune(0xDFFF)
+    when not defined(c_strlen):
+      proc c_strlen(a: cstring): int {.nodecl, noSideEffect, importc: "strlen".}
 
-    proc allocWCS(s: string, len: int): WideCString =
-      ## free after usage with `dealloc`.
-      result = cast[WideCString](alloc(len * 4 + 2))
-      var d = 0
-      for ch in runes(s):
-        if ch <=% UNI_MAX_BMP:
-          if ch >=% UNI_SUR_HIGH_START and ch <=% UNI_SUR_LOW_END:
+    when true: # optimized procedure
+
+      proc allocWCS(s: string): WideCString =
+        ## free after usage with `dealloc`.
+        if s.isNil: return nil
+        let len = c_strlen(s)
+        result = cast[WideCString](alloc(len * 4 + 2))
+        var d = 0
+        for ch in runes(s):
+          result[d] = TUTF32Char(ch)
+          inc d
+        result[d] = TUTF32Char(0'i32)
+
+    else:
+      const
+        UNI_REPLACEMENT_CHAR = TUTF32Char(0xFFFD'i32)
+        UNI_MAX_BMP = TRune(0x0000FFFF)
+        UNI_MAX_UTF32 = TRune(0x7FFFFFFF)
+        #UNI_MAX_LEGAL_UTF32 = TRune(0x0010FFFF)
+        halfShift = 10
+        halfBase = 0x0010000
+        halfMask = 0x3FF
+        UNI_SUR_HIGH_START = TRune(0xD800)
+        #UNI_SUR_HIGH_END = TRune(0xDBFF)
+        UNI_SUR_LOW_START = TRune(0xDC00)
+        UNI_SUR_LOW_END = TRune(0xDFFF)
+
+      proc allocWCS(s: string): WideCString =
+        ## free after usage with `dealloc`.
+        if s.isNil: return nil
+        let len = c_strlen(s)
+        result = cast[WideCString](alloc(len * 4 + 2))
+        var d = 0
+        for ch in runes(s):
+          if ch <=% UNI_MAX_BMP:
+            if ch >=% UNI_SUR_HIGH_START and ch <=% UNI_SUR_LOW_END:
+              result[d] = UNI_REPLACEMENT_CHAR
+            else:
+              result[d] = TUTF32Char(ch)
+          elif ch >% UNI_MAX_UTF32:
             result[d] = UNI_REPLACEMENT_CHAR
           else:
-            result[d] = TUTF32Char(ch)
-        elif ch >% UNI_MAX_UTF32:
-          result[d] = UNI_REPLACEMENT_CHAR
-        else:
-          let ch = int32(ch) -% halfBase
-          result[d] = TUTF32Char((ch shr halfShift) +% int32(UNI_SUR_HIGH_START))
+            let ch = int32(ch) -% halfBase
+            result[d] = TUTF32Char((ch shr halfShift) +% int32(UNI_SUR_HIGH_START))
+            inc d
+            result[d] = TUTF32Char((ch and halfMask) +% int32(UNI_SUR_LOW_START))
           inc d
-          result[d] = TUTF32Char((ch and halfMask) +% int32(UNI_SUR_LOW_START))
-        inc d
-      result[d] = TUTF32Char(0'i32)
-
-    proc allocWCS(s: string): WideCString =
-      ## free after usage with `dealloc`.
-      if s.isNil: return nil
-
-      when not defined(c_strlen):
-        proc c_strlen(a: cstring): int {.nodecl, noSideEffect, importc: "strlen".}
-
-      let len = c_strlen(s)
-      result = allocWCS(s, len)
+        result[d] = TUTF32Char(0'i32)
 
   else: # windows
     template allocWCS(s: string): WideCString =
